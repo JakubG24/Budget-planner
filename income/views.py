80b02f60b97a19a858f2c9f5f4058d8ce476d7dd
JakubG24 <jakub.gierczak24@gmail.com>
2021-05-17 20:19:27 +0200

    paginator

--- income/views.py
+++ income/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 
@@ -11,5 +12,8 @@ class IndexView(View):
 
 class IncomeView(View):
     def get(self, request):
-        incomes = Income.objects.all()
-        return render(request, 'income/income_view.html', {'incomes': incomes})
+        income = Income.objects.filter(user=request.user).order_by('date')
+        paginator = Paginator(income, 1)
+        page_number = request.GET.get('page')
+        page = paginator.get_page(page_number)
+        return render(request, 'income/income_view.html', {'income_list': income, 'pages': page})
