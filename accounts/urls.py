5775dad3d2f014500a1f1b01c6facd00c48817d6
JakubG24 <jakub.gierczak24@gmail.com>
2021-05-17 17:28:32 +0200

    user change password

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,7 @@ from accounts import views
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('settings/', views.AccountSettingsView.as_view(), name='account_settings'),
+    path('change_password/', views.ChangePasswordView.as_view(), name='change_password'),
 
 ]
