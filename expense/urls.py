16701b13d369e8bfece761da0b1b0a6d5d4d1a65
JakubG24 <jakub.gierczak24@gmail.com>
2021-05-21 18:59:10 +0200

    updateview

--- expense/urls.py
+++ expense/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('fixed_costs/', views.FixedCostView.as_view(), name='fixed_cost_view'),
     path('add_expense/fixed/', views.AddFixedCostView.as_view(), name='add_fixed'),
     path('create_category/fixed/', views.CreateCategoryView.as_view(), name='create_category_fixed'),
-    path('fixed_costs/edit/<int:id>/', views.EditFixedCostView.as_view(), name='edit_fixed'),
+    path('fixed_costs/edit/<int:pk>/', views.EditFixedCostView.as_view(), name='edit_fixed'),
     path('variable_costs/', views.VariableCostView.as_view(), name='variable_cost_view'),
     path('ajax/load-sources/', views.load_sources, name='ajax_load_sources'),
 
