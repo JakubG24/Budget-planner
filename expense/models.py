4e3236ad16ec78ac41cb58f261640ac68cc32a32
JakubG24 <jakub.gierczak24@gmail.com>
2021-05-21 20:46:21 +0200

    variables

--- expense/models.py
+++ expense/models.py
@@ -35,19 +35,33 @@ class FixedCosts(models.Model):
         return self.date.strftime("%Y-%m-%d")
 
 
-class VariableCostSource(models.Model):
-    name = models.CharField(max_length=64)
-
-
 class VariableCostSourceCategory(models.Model):
     name = models.CharField(max_length=64)
-    user_id = models.ForeignKey(User, on_delete=CASCADE)
-    source = models.ManyToManyField(FixedCostSource)
+    user = models.ForeignKey(User, on_delete=CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class VariableCostSource(models.Model):
+    name = models.CharField(max_length=64)
+    user = models.ForeignKey(User, on_delete=CASCADE, null=True)
+    source = models.ForeignKey(VariableCostSourceCategory, on_delete=CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class VariableCosts(models.Model):
     amount = models.FloatField()
     date = models.DateField()
     description = models.TextField(max_length=128)
-    user = models.ForeignKey(User, on_delete=CASCADE)
-    source = models.ForeignKey(FixedCostSource, on_delete=CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(VariableCostSourceCategory, on_delete=models.SET_NULL, null=True)
+    source = models.ForeignKey(VariableCostSource, on_delete=models.SET_NULL, null=True)
+
+    def get_absolute_url(self):
+        return f'/expense/variable_costs/edit/{self.id}/'
+
+    def get_date(self):
+        return self.date.strftime("%Y-%m-%d")
